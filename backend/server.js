a02f726a5de230cc74c7ef21d5d07c94c85fe53a
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-24 23:05:02 -0400

    Fix deployment issue

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 require("dotenv").config();
 
 const express = require("express");
@@ -20,6 +21,17 @@ app.use((req, res, next) => {
 
 app.use("/api/entertainment", entertainmentRoutes);
 app.use("/api/user", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 mongoose
   .connect(process.env.MONGO_URI)
