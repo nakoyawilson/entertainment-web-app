f04102322a6892d48746a9f78ac22375398bbae1
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-24 23:30:03 -0400

    Make minor adjustments

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,14 @@ const userRoutes = require("./routes/user");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
-
 app.use(express.json());
-
-// log out the requests coming in
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
 
 app.use("/api/entertainment", entertainmentRoutes);
 app.use("/api/user", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
