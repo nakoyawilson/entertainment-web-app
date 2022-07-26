b75c8f9491bbc2aa36787e945ef9c4c347a805a5
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-26 19:39:02 -0400

    Delete unnecessary scripts

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,16 @@ app.use("/api/user", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("/*", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
 }
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(port, () => {
-      console.log(`connected to db and listening on port ${port}`);
+      console.log(`connected to db & listening on port ${port}`);
     });
   })
   .catch((error) => {
