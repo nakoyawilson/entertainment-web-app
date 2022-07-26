f7b66a3503adbf16fa612a69d11c1e386ce72357
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-26 06:51:32 -0400

    Create userController and userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,27 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ _id: id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
 
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,6 +14,11 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 userSchema.statics.signup = async function (email, password) {
@@ -33,6 +38,24 @@ userSchema.statics.signup = async function (email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   const user = await this.create({ email, password: hash });
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
   return user;
 };
 
