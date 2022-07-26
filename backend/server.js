8b3e89a778e690023aade9590f72b3fe25c289d4
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-26 19:46:06 -0400

    Fix deployment issue using path.resolve()

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,14 @@ app.use(express.json());
 app.use("/api/entertainment", entertainmentRoutes);
 app.use("/api/user", userRoutes);
 
+const resolvedDirectory = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(resolvedDirectory, "/frontend/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(resolvedDirectory, "frontend", "build", "index.html")
+    )
   );
 }
 
